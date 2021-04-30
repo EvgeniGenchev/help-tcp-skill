fd64d1ac28ce9ac928943738ef66d37b59a8b685
EvgeniGenchev <evgena.genchev@tutanota.com>
2021-04-30 17:25:36 +0100

    initial commit

--- __init__.py
+++ __init__.py
@@ -1,5 +1,10 @@
 from mycroft import MycroftSkill, intent_file_handler
+import socket
+from datetime import datetime
 
+HOST = '192.168.2.7'
+PORT = 80
+DEVICE_ID = 123653254
 
 class HelpTcp(MycroftSkill):
     def __init__(self):
@@ -7,6 +12,11 @@ class HelpTcp(MycroftSkill):
 
     @intent_file_handler('tcp.help.intent')
     def handle_tcp_help(self, message):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as c:
+            c.connect((HOST, PORT))
+            alert = "Help " + str(DEVICE_ID) + " " + str(datetime.now())
+            c.send(alert.encode('utf-8'))
+
         self.speak_dialog('tcp.help')
 
 
